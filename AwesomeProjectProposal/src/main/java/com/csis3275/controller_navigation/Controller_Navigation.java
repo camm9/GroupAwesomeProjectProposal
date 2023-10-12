b7403b1cbba03819142964396a13b5f2ca8bfc3b
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-10-12 12:34:01 -0700

    feat: Added new mapping to login page

--- AwesomeProjectProposal/src/main/java/com/csis3275/controller_navigation/Controller_Navigation.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/controller_navigation/Controller_Navigation.java
@@ -1,14 +1,19 @@
-package com.csis3275.controller_home;
+package com.csis3275.controller_navigation;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Controller_Home {
+public class Controller_Navigation {
 	
 	@GetMapping("/")
 	public String renderHome() {
 		return "home";
+	}
+	
+	@GetMapping("/login")
+	public String renderLogin() {
+		return "login";
 	}
 
 }

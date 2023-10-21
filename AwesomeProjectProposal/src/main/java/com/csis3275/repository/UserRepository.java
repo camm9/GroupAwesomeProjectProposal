3d24156e0a168cef40dc5372275313beef92cefa
Edison CHAN <chanw30@student.douglascollege.ca>
2023-10-20 17:49:25 -0700

    [bug fix] Removed unfinished item from Jamin's branch that was pulled by accident

--- AwesomeProjectProposal/src/main/java/com/csis3275/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.csis3275.repository;
-
-import com.csis3275.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}

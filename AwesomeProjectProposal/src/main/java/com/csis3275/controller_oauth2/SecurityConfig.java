5b0719f9a23449247d534bd571b8f5423adf86df
Edison CHAN <chanw30@student.douglascollege.ca>
2023-10-24 10:08:16 -0700

    Updated Spring Secutiy Config

---
+++ AwesomeProjectProposal/src/main/java/com/csis3275/controller_oauth2/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.csis3275.controller_oauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/", "/login").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+}

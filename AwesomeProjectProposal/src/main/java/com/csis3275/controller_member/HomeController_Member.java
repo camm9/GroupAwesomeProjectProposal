325499fba60b2cf56b2fc1faf44f4835cda71720
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-10-27 22:57:38 -0700

    fix: Added new PostMapping to retrieve data selected by users

--- AwesomeProjectProposal/src/main/java/com/csis3275/controller_member/HomeController_Member.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/controller_member/HomeController_Member.java
@@ -1,7 +1,12 @@
 package com.csis3275.controller_member;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController_Member {
@@ -9,6 +14,18 @@ public class HomeController_Member {
 	@GetMapping("/member")
 	public String renderHomeMmeber() {
 		return "member/index";
+	}
+	
+	@GetMapping("/member/mainstat")
+	public String renderStatMember() {
+		return "member/mainstat";
+	}
+
+	
+	@PostMapping("/member/api_date")
+	public String renderAPIDateMember(@RequestParam("date_p") String date_p) {
+		System.out.print(date_p);
+		return "member/mainstat";
 	}
 
 }

7f9cdb8a098527b6053cd1542c97e17ed56e0e58
camm9 <student.camm@gmail.com>
2023-10-28 17:25:39 -0700

    Feat: created getAllMatchesForDate(String date) that returns a list of Match(String matchID, String homeTeam, String awayTeam) to feed into HTML

--- AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
@@ -1,27 +1,30 @@
 package com.csis3275;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+import com.csis3275.model_api.Match;
 import com.csis3275.service_api.*;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @SpringBootApplication
 public class AwesomeProjectProposalApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(AwesomeProjectProposalApplication.class);
 	static APIService apiService = new APIService();
+	static List<Match> listOfMatches = new ArrayList();
 	
 	
 	public static void main(String[] args) {
 		log.info("In main method");
 		SpringApplication.run(AwesomeProjectProposalApplication.class, args);
 		try {
-			apiService.getAllPredictionDataForDate();
-//			apiService.getFederationList();
+			listOfMatches = apiService.getAllMatchesForDate("2023-10-28");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

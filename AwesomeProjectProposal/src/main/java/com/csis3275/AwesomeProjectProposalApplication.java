50dfd4da5f31cf187a79d3895f1c97677c876d87
Edison CHAN <chanw30@student.douglascollege.ca>
2023-10-31 11:04:15 -0700

    [Feature] Restored GET API function to demo api funtionalities

--- AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
@@ -13,30 +13,29 @@ import com.csis3275.service_db.UserService;
 
 @SpringBootApplication
 public class AwesomeProjectProposalApplication {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(AwesomeProjectProposalApplication.class);
 	static APIService apiService = new APIService();
-	
-	
+
 	public static void main(String[] args) {
 		log.info("In main method");
 		SpringApplication.run(AwesomeProjectProposalApplication.class, args);
 		try {
-//			apiService.getAllPredictionDataForDate();
-//			apiService.getFederationList();
+			apiService.getAllPredictionDataForDate();
+			apiService.getFederationList();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Bean
-	CommandLineRunner userDB(UserService repo)	{
+	CommandLineRunner userDB(UserService repo) {
 		return (args) -> {
-		repo.saveUser(new User("testusername", "test@email.com", "testpassword"));
-		repo.saveUser(new User("test2username", "test2@email.com", "test2password"));
+			repo.saveUser(new User("testusername", "test@email.com", "testpassword"));
+			repo.saveUser(new User("test2username", "test2@email.com", "test2password"));
 		};
-		
+
 	}
 
 }

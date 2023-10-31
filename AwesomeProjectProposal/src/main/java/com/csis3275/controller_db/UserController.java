c7edeeb7fb7ca62e901ecab34fbf44fb2705a4b3
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-10-30 23:13:20 -0700

    fix: Fixed links and styles

--- AwesomeProjectProposal/src/main/java/com/csis3275/controller_db/UserController.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/controller_db/UserController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.csis3275.model_db.User;
 import com.csis3275.service_db.UserService;
 
 @Controller
@@ -19,11 +21,24 @@ public class UserController {
 		model.addAttribute("userList", userService.readUsers());
 		return "/admin/index";
 	}
+	
+	@GetMapping("/admin/add")
+	public String addStudentAdmin() {
+		return "/admin/addadmin";
+	}
 
 	@GetMapping("/users/delete")
 	public String deleteUser(@RequestParam("deleteUser") String id) {
 		// Delete the student
 		userService.deleteUser(Long.parseLong(id));
+		return "redirect:/admin";
+	}
+	
+	@PostMapping("/admin/saveadmin")
+	public String renderSaveMember(User newAdmin) {
+		//Create Admin
+		
+		userService.saveUser(newAdmin);
 		return "redirect:/admin";
 	}
 

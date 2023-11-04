a313e1299772de00325e76dea724b5f4c81f1e22
camm9 <student.camm@gmail.com>
2023-11-04 11:49:01 -0700

    FEAT: fed getAllMatchesForDate(String date) into table on mainstat page

--- AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/AwesomeProjectProposalApplication.java
@@ -2,16 +2,12 @@ package com.csis3275;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-
-
 import com.csis3275.model_api.Datum;
 import com.csis3275.service_api.*;
 import com.fasterxml.jackson.databind.JsonNode;

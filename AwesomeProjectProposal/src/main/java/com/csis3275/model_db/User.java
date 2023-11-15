92bfd1479bc0539255eb4c371ec82d3b26b22fb4
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-11-14 21:56:57 -0800

    Added user field to create a properly relationship

--- AwesomeProjectProposal/src/main/java/com/csis3275/model_db/User.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/model_db/User.java
@@ -1,9 +1,16 @@
 package com.csis3275.model_db;
 
+import java.util.List;
+import java.util.Set;
+import org.hibernate.annotations.Cascade;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,18 +20,34 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO )
 	private Long userId;
+	@Column(name = "username", nullable = true)
 	private String username;
+	@Column(name = "email", nullable = true)
 	private String email;
+	@Column(name = "password", nullable = true)
 	private String password;
+	@Column(name = "tokenUser", nullable = true)
+	private String tokenUser;
+	
+	
+	// Mapping to the other table
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<UserDataset> user;
+	
 	
 	public User(){
 		
 	}
 	
-	public User(String username, String email, String password) {
+	public User(String tokenUser){
+		this.tokenUser = tokenUser;
+	}
+	
+	public User(String username, String email, String password, String tokenUser) {
 		this.username = username;
 		this.email = email;
 		this.password = password;
+		this.tokenUser = tokenUser;
 	}
 	
 	public Long getUserId() {
@@ -53,5 +76,16 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getTokenUser() {
+		return tokenUser;
+	}
+
+	public void setTokenUser(String tokenUser) {
+		this.tokenUser = tokenUser;
+	}
+
+	
+	
 
 }

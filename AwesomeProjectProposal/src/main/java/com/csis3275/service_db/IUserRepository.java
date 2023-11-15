988136ebc20f526c26366a2bb6108832a35473d6
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-11-14 22:07:05 -0800

    fix: Deleted spaces, no changes

--- AwesomeProjectProposal/src/main/java/com/csis3275/service_db/IUserRepository.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/service_db/IUserRepository.java
@@ -1,5 +1,6 @@
 package com.csis3275.service_db;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.csis3275.model_db.User;

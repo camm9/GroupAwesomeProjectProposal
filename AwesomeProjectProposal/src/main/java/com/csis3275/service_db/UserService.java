0130aa678fd78421035e0b9603ce9f53b7158b85
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-11-14 21:59:46 -0800

    fix: Changed structure and added comments

--- AwesomeProjectProposal/src/main/java/com/csis3275/service_db/UserService.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/service_db/UserService.java
@@ -1,9 +1,16 @@
 package com.csis3275.service_db;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 import org.springframework.stereotype.Service;
 
 import com.csis3275.model_db.User;
@@ -54,12 +61,7 @@ public class UserService implements IUserRepository {
 			return null;
 		}
 
-		@Override
-		public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
+		
 		@Override
 		public Optional<User> findById(Long id) {
 			// TODO Auto-generated method stub
@@ -72,17 +74,7 @@ public class UserService implements IUserRepository {
 			return false;
 		}
 
-		@Override
-		public Iterable<User> findAll() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public Iterable<User> findAllById(Iterable<Long> ids) {
-			// TODO Auto-generated method stub
-			return null;
-		}
+		
 
 		@Override
 		public long count() {
@@ -119,4 +111,24 @@ public class UserService implements IUserRepository {
 			// TODO Auto-generated method stub
 			
 		}
+
+		@Override
+		public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Iterable<User> findAll() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Iterable<User> findAllById(Iterable<Long> ids) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		
 }

99cd7701456bacf6b020b075323fe75f95d9a0a4
Edison CHAN <chanw30@student.douglascollege.ca>
2023-11-21 02:20:59 -0800

    Feature: CSV Generator

---
+++ AwesomeProjectProposal/src/main/java/com/csis3275/service_csv/CsvFileGenerator.java
@@ -0,0 +1,25 @@
+package com.csis3275.service_csv;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.springframework.stereotype.Component;
+
+import com.csis3275.model_db.UserDataset;
+
+@Component
+public class CsvFileGenerator {
+	public void writeStudentsToCsv(List<UserDataset> UserDatasets, Writer writer) {
+		try {
+
+			CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT);
+			for (UserDataset UserDataset : UserDatasets) {
+				printer.printRecord(UserDataset.getUser(), UserDataset.getMatchId(), UserDataset.getDos());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

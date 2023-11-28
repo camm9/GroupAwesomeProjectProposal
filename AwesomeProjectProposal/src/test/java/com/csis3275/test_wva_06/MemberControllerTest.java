9a22a71525a512494b516f4465036d7dadf0df08
Washington Valencia <valenciaasquiw@student.douglascollege.ca>
2023-11-28 12:19:41 -0800

    test: Added new Junit test

--- AwesomeProjectProposal/src/test/java/com/csis3275/test_wva_06/MemberControllerTest.java
+++ AwesomeProjectProposal/src/test/java/com/csis3275/test_wva_06/MemberControllerTest.java
@@ -8,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.csis3275.controller_member.*;
 
 
@@ -40,18 +43,17 @@ public class MemberControllerTest {
 	 */
 	
 	@Test
+	@DisplayName("Verify Member Space Web Page with 302 status")
+	/**"Should display an 302 error in member Space Web page and redirect to login page."**/
 	void TestMainStatPage() throws Exception	{
+		
+				
 		mvc.perform(MockMvcRequestBuilders
 				.get("/member/mainstat")
 				.accept(MediaType.TEXT_HTML)) //From springframework.http
 				.andDo(print())
-				.andExpect(model().attributeExists("matchListSize"))
-				.andExpect(model().attributeExists("preferedDataSet"))
-				.andExpect(model().attributeExists("dateTime"))
-				.andExpect(model().attributeExists("dateArray"))
-				.andExpect(model().attributeExists("occurrences"))
-				.andExpect(status().isOk())
-				.andExpect(MockMvcResultMatchers.view().name("member/mainstat"));
+				.andExpect(status().is(302))
+				.andExpect(MockMvcResultMatchers.view().name("redirect:/loginpage"));
 	}
 	
 	
@@ -59,6 +61,8 @@ public class MemberControllerTest {
 	 * This test will check the HomeController_Member specifically renderValidarUser() method to check the model attributes are present.
 	 */
 	@Test
+	@DisplayName("Verify tokenuser attribute for Members with 302 status")
+	/**"Should verify a tokenuser attribute for Members without errors"**/
 	void TestRenderValidarUser() throws Exception	{
 		
 		
@@ -68,12 +72,15 @@ public class MemberControllerTest {
 				.andDo(print())
 				.andExpect(MockMvcResultMatchers.view().name("redirect:/member"))
 				.andExpect(model().attribute("tokenuser", nullValue()))
-				.andExpect(status().isOk());
-				
+				.andExpect(status().is(302));			
 	}
 	
-
+	/**
+	 * This test will check the HomeController_Member specifically renderSaveDatasetMember() method to check the model attributes are present.
+	 */
 	@Test
+	@DisplayName("Verify a user is logged")
+	/**"Based on tokenuser attribute members should be redirect to Login page or Dashboard Web page."**/	
 	void TestRenderSaveDatasetMember() throws Exception	{
 
 		mvc.perform(MockMvcRequestBuilders
@@ -84,11 +91,74 @@ public class MemberControllerTest {
 				.andExpect(status().isOk());
 	}
 	
+	/**
+	 * This test will check the HomeController_Member specifically renderSaveDatasetMember() method to check the model attributes are present.
+	 */
+	@Test
+	@DisplayName("Save a specific dataset on DB")
+	/**"The test should save a specific data set selected by users and identify the parameters matchID, token, date1, date2."**/	
+	void TestRenderSaveDatasetMemberDB() throws Exception	{
+
+		// sample random string
+		String rnd = "oi1rf3gpaiobaqdkp5ih045uusc6rgfqe9fr4sl9rtnki8ejrj52f34pme4mptfqcsn698d7790c1c4g4kir2h2cvsimtat7vb9v";
+		// sample matchID take from API (valid)
+		String matchid = "277799";
+		// sample tokenID
+		String token = "2";
+		// sample Date of search
+		String dos = "2023-11-26";
+		// sample Date of match 
+		String date_p = "2023-11-26";
+		
+		mvc.perform(MockMvcRequestBuilders
+				.get("/member")
+				.param("rnd", rnd)
+				.param("matchid", matchid)
+				.param("token", token)
+				.param("dos", dos)
+				.accept(MediaType.TEXT_HTML)) //From springframework.http
+				.andDo(print())
+				.andExpect(MockMvcResultMatchers.view().name("/loginpage"))
+				.andExpect(status().isOk());
+	}
+	
+	
+	@Test
+	@DisplayName("Verify dahsboard Web page")
+	/**"Should display dahsboard Web page without errors"**/	
+	void TestEenderAPIDateMember2() throws Exception	{
+
+		mvc.perform(MockMvcRequestBuilders
+				.get("/member")
+				.accept(MediaType.TEXT_HTML)) //From springframework.http
+				.andDo(print())
+				.andExpect(MockMvcResultMatchers.view().name("/loginpage"))
+				.andExpect(status().isOk());
+	}
+	
+	
+	@Test
+	@DisplayName("Get statistics details using a matchID from Dashboard")
+	/**"Should display details of a specific matchID, which is passed by get parameter."**/	
+	void TestRenderMatchIDDetails() throws Exception	{
+
+		// sample matchID take from API (valid)
+		String matchID = "277799";
+				
+		mvc.perform(MockMvcRequestBuilders
+				.get("/member/matchIDdetails")
+				.param("matchID", matchID)
+				.accept(MediaType.TEXT_HTML)) //From springframework.http
+				.andDo(print())
+				.andExpect(model().attribute("matchID","277799"))
+				.andExpect(MockMvcResultMatchers.view().name("member/matchdetails"))
+				.andExpect(status().isOk());
+	}
+	
+	
 	
 	
 
-
-	
 	
 	
 }

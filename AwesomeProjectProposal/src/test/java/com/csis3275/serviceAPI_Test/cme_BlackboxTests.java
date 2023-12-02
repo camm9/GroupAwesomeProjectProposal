3dc0ea3d063cc42e620dbc14696969f6f5b2a098
camm9 <student.camm@gmail.com>
2023-12-02 14:49:42 -0800

    TEST: added mvn dependency and creating initial tests for Selenium

---
+++ AwesomeProjectProposal/src/test/java/com/csis3275/serviceAPI_Test/cme_BlackboxTests.java
@@ -0,0 +1,58 @@
+package com.csis3275.serviceAPI_Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class cme_BlackboxTests {
+
+	private static FirefoxDriver driver;
+	
+	@BeforeAll
+	public static void setUp() {
+		//Set up browser
+		System.setProperty("webdriver.gecko.driver", "A:\\Douglas\\csis3275\\geckodriver.exe");
+		FirefoxOptions options = new FirefoxOptions();
+		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+		options.setImplicitWaitTimeout(Duration.ofSeconds(10));
+		driver = new FirefoxDriver(options);
+	}
+	
+	@AfterAll
+	public static void tearDown()	{
+		//Quit the browser
+		driver.quit();
+	}
+	
+	
+	@Test
+	@DisplayName("User searches for match stats")
+	  public void searchMatchStats() {
+	    driver.get("http://localhost:8080/");
+	    driver.manage().window().maximize();
+	    driver.findElement(By.cssSelector(".ml-2")).click();
+	    driver.findElement(By.linkText("GitHub")).click();
+	    driver.findElement(By.linkText("Match Details")).click();
+	    driver.findElement(By.id("matchDetailsByIDValue")).click();
+	    driver.findElement(By.id("matchDetailsByIDValue")).sendKeys("279290");
+	    driver.findElement(By.id("matchDetailsByID_button")).click();
+	    driver.quit();
+	  }
+	
+//	@Test
+//	@DisplayName("User searches for match odds")
+//	  public void searchMatchOdds() {
+//	    driver.get("http://localhost:8080/");
+//	  }
+
+}

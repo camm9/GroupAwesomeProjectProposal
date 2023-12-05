50efef563f992576351ffd4ad4abe8ce75a703dd
Edison CHAN <chanw30@student.douglascollege.ca>
2023-12-05 03:44:28 -0800

    Feature: Added auth controller and success manager to control redirection

--- AwesomeProjectProposal/src/main/java/com/csis3275/controller_auth/AuthController.java
+++ AwesomeProjectProposal/src/main/java/com/csis3275/controller_auth/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.csis3275.model_db.User;
+import com.csis3275.securityconfig.LoginSuccessHandler;
 import com.csis3275.service_db.UserService;
 
 @Controller
@@ -15,10 +16,19 @@ public class AuthController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/login/oauth2/code/github")
-	public String login() {
-		// Delete the student
-		//userService.deleteUser(Long.parseLong(id));
-		return "redirect:/member/valida";
+	@GetMapping("/login/member")
+	public String loginMember() {
+		LoginSuccessHandler.State = 0;
+		//return "http://localhost:8080/oauth2/authorization/github";
+		return "redirect:/oauth2/authorization/github";
+		//return "redirect:/member/valida";
+	}
+	
+	@GetMapping("/login/admin")
+	public String loginAdmin() {
+		LoginSuccessHandler.State = 1;
+		//model.addAttribute("userList", userService.readUsers());
+		//return "/admin/index";
+		return "redirect:/oauth2/authorization/github";
 	}
 }
---
+++ AwesomeProjectProposal/src/main/java/com/csis3275/securityconfig/LoginSuccessHandler.java
@@ -0,0 +1,7 @@
+package com.csis3275.securityconfig;
+
+public class LoginSuccessHandler {
+	public static int State = 0; //0 is user, 1 is admin
+	
+	
+}
